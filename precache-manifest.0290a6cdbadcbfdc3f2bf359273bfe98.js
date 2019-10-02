898adff6205482f8ecac2b7292eb7126a13da2ad
Léo Huppert <leo@huppert.io>
2019-10-02 23:24:37 +0200

    Updates

---
+++ precache-manifest.0290a6cdbadcbfdc3f2bf359273bfe98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8633600df278b18d2beb671706561e",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "73b76ad8e58b052c2d7d",
+    "url": "/todolist/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d3a44416622a5e05c952",
+    "url": "/todolist/static/js/2.7ddaa1f3.chunk.js"
+  },
+  {
+    "revision": "73b76ad8e58b052c2d7d",
+    "url": "/todolist/static/js/main.120d38a1.chunk.js"
+  },
+  {
+    "revision": "4e1b8ed3c740f7eb5649",
+    "url": "/todolist/static/js/runtime-main.1395c765.js"
+  }
+]);
