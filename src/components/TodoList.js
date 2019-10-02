623b98e62d37144ad745e20307b061353ac8db5b
Léo Huppert <leo@huppert.io>
2019-10-02 22:34:29 +0200

    Cache todos in LocalStorage

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,25 +7,41 @@ export default class TodoList extends React.Component {
     todos: [],
   };
 
+  componentDidMount() {
+    const cachedTodos = JSON.parse(localStorage.getItem("todos"));
+
+    this.setState({ todos: cachedTodos });
+  }
+
   addTodo = ({ id, name, complete }) => {
-    this.setState({
-      todos: [{ id, name, complete }, ...this.state.todos],
-    });
+    this.setState(
+      {
+        todos: [{ id, name, complete }, ...this.state.todos],
+      },
+      () => {
+        localStorage.setItem("todos", JSON.stringify(this.state.todos));
+      }
+    );
   };
 
   completeTodo = id => {
-    this.setState(state => ({
-      todos: state.todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            complete: !todo.complete,
-          };
-        } else {
-          return todo;
-        }
+    this.setState(
+      state => ({
+        todos: state.todos.map(todo => {
+          if (todo.id === id) {
+            return {
+              ...todo,
+              complete: !todo.complete,
+            };
+          } else {
+            return todo;
+          }
+        }),
       }),
-    }));
+      () => {
+        localStorage.setItem("todos", JSON.stringify(this.state.todos));
+      }
+    );
   };
 
   render() {
